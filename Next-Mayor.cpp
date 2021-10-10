4bfe84e27856334d93c37933b6ca5e705b45a654
quantumshiro <hiro.1nakanishi@gmail.com>
2021-10-10 14:32:58 +0900

    solve now

---
+++ Next-Mayor.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	while(true) {
+		int n, p;
+		cin >> n >> p;
+
+		if (n == 0 and p == 0) break;
+		// n -> 候補者
+		// p -> 小石の総数
+	}
+}
