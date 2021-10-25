441e4834b07f511bd393b0cf90023c4b9d2a6adf
quantumshiro <hiro.1nakanishi@gmail.com>
2021-10-25 16:22:27 +0900

    solved

---
+++ Hanafuda-Shuffle.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    while (true) {
+        int n, r;
+        cin >> n >> r;
+        if (n == 0 and r == 0)
+            break;
+        
+    }
+}
